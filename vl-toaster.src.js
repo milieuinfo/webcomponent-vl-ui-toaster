55248a134788cae4f5bca699d45b49b87e29a803
zhaodifeng <zhaodi.feng@vlaanderen.be>
2019-08-05 16:38:19 +0200

    init

---
+++ vl-toaster.src.js
@@ -0,0 +1,78 @@
+import {NativeVlElement} from "/node_modules/vl-ui-core/vl-core.js";
+
+(() => {
+  loadScript('util.js', '/node_modules/@govflanders/vl-ui-util/dist/js/util.min.js', () => {
+    loadScript('core.js', '/node_modules/@govflanders/vl-ui-core/dist/js/core.min.js', () => {
+      loadScript('vl-toaster.js', '/node_modules/@govflanders/vl-ui-toaster/dist/js/toaster.js');
+    });
+  });
+
+  function loadScript(id, src, onload) {
+    if (!document.head.querySelector('#' + id)) {
+      let script = document.createElement('script');
+      script.setAttribute('id', id);
+      script.setAttribute('src', src);
+      script.onload = onload;
+      document.head.appendChild(script);
+    }
+  }
+})();
+
+export class VlToaster extends NativeVlElement(HTMLDivElement) {
+  constructor(){
+    super();
+  }
+
+  get _classPrefix() {
+    return 'vl-toaster--';
+  }
+
+  get _stylePath() {
+    return '../style.css';
+  }
+
+  _fadeoutChangedCallback(oldValue, newValue) {
+    this.setAttribute('data-vl-toaster','');
+    this.setAttribute('data-vl-toaster-fadeout', true);
+    this.dress();
+  };
+
+  static get _observedAttributes() {
+    return ['fadeout'];
+  }
+
+  static get _observedClassAttributes() {
+    return ['top-left', 'top-right', 'bottom-left', 'bottom-right']
+  }
+
+  static get _dressedAttributeName() {
+    return 'data-vl-select-dressed';
+  }
+
+  get _dressed() {
+    return !!this.getAttribute(VlToaster._dressedAttributeName);
+  }
+
+  dress() {
+    (async () => {
+      while(!window.vl || !window.vl.toaster) {
+        await new Promise(resolve => setTimeout(resolve, 100));
+      }
+      if (!this._dressed) {
+        vl.toaster.dress(this);
+      }
+    })();
+  }
+
+  connectedCallback(){
+    this.classList.add('vl-toaster');
+    this.dress();
+  }
+
+  addAlert(alert){
+    this._element.appendChild(alert);
+  }
+
+}
+
+customElements.define('vl-toaster', VlToaster, {extends: 'div'});
