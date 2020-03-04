84ac972da2e2304d92b9f49954257ede836e2e5e
Sander Kleykens <sander.kleykens@avnu.be>
2020-03-04 22:03:20 +0100

    Introduceer e2e testen

--- test/e2e/components/vl-toaster.js
+++ test/e2e/components/vl-toaster.js
@@ -1,7 +1,37 @@
 const { VlElement } = require('vl-ui-core').Test;
+const { By } = require('vl-ui-core').Test.Setup;
 
-class VlToaster extends VlElement {  
-  
+class VlToaster extends VlElement {
+    async shouldFadeOut() {
+        return this.hasAttribute('fadeout');
+    }
+
+    async isLocatedTopLeft() {
+        return this.hasAttribute('top-left');
+    }
+
+    async isLocatedTopRight() {
+        return this.hasAttribute('top-right');
+    }
+
+    async isLocatedBottomLeft() {
+        return this.hasAttribute('bottom-left');
+    }
+
+    async isLocatedBottomRight() {
+        return this.hasAttribute('bottom-right');
+    }
+
+    async getAlerts() {
+        const children = await this.findElements(By.css('vl-alert'));
+        if(children.length > 0) {
+            const { VlAlert } = require('vl-ui-alert').Test;
+            return Promise.all(children.map(child => new VlAlert(this.driver, child)));
+        } else {
+            return [];
+        }
+    }
 }
+
 
 module.exports = VlToaster;
