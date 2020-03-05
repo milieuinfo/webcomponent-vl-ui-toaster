71daa8c5d196fd4e1d0b9adc3937064a71a5d30d
coemans <tom.coemans@gmail.com>
2020-03-05 13:48:01 +0100

    Probeer getAlerts() opnieuw indien er zich een stale element reference error voordoet tijdens het omzetten naar VlAlerts (#64)

--- test/e2e/components/vl-toaster.js
+++ test/e2e/components/vl-toaster.js
@@ -1,5 +1,6 @@
 const { VlElement } = require('vl-ui-core').Test;
 const { By } = require('vl-ui-core').Test.Setup;
+const { StaleElementReferenceError } = require('selenium-webdriver/lib/error');
 
 class VlToaster extends VlElement {
     async shouldFadeOut() {
@@ -26,7 +27,16 @@ class VlToaster extends VlElement {
         const children = await this.findElements(By.css('vl-alert'));
         if (children.length > 0) {
             const { VlAlert } = require('vl-ui-alert').Test;
-            return Promise.all(children.map(child => new VlAlert(this.driver, child)));
+
+            try {
+                return await Promise.all(children.map(child => new VlAlert(this.driver, child)));
+            } catch (error) {
+                if (error instanceof StaleElementReferenceError) {
+                    return this.getAlerts();
+                } else {
+                    throw error;
+                }
+            }
         } else {
             return [];
         }
